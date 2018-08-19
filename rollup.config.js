337dda57dfe086a3be653e384d004532358793f9
Eduard Kyvenko <edu@june.dk>
2018-08-19 20:59:29 +0200

    Enable trailing semicolons

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,29 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
-import { uglify } from 'rollup-plugin-uglify'
-import pkg from './package.json'
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import { uglify } from 'rollup-plugin-uglify';
+import pkg from './package.json';
 
-const input = './lib/index.js'
-const name = 'ReactInfiniteRange'
+const input = './lib/index.js';
+const name = 'ReactInfiniteRange';
 const globals = {
   react: 'React'
-}
+};
 
 // treat as external "module/path" modules and reserved rollup paths
 const external = id =>
-  !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/')
+  !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/');
 
 const getBabelOptions = ({ useESModules }) => ({
   exclude: '**/node_modules/**',
   runtimeHelpers: true,
   plugins: [['@babel/transform-runtime', { useESModules }]]
-})
+});
 
 const commonjsOptions = {
   include: '**/node_modules/**'
-}
+};
 
 export default [
   {
@@ -74,4 +74,4 @@ export default [
     external,
     plugins: [babel(getBabelOptions({ useESModules: false }))]
   }
-]
+];
