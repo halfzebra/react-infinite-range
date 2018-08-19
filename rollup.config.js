9be2df0c9516c3863cf3ceb3306598da2d3a11a1
Eduard Kyvenko <edu@june.dk>
2018-08-19 20:43:49 +0200

    Added prettier and format the code.

--- rollup.config.js
+++ rollup.config.js
@@ -1,68 +1,77 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-import { uglify } from 'rollup-plugin-uglify';
-import pkg from './package.json';
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import { uglify } from 'rollup-plugin-uglify'
+import pkg from './package.json'
 
-const input = './lib/index.js';
-const name = 'ReactInfiniteRange';
+const input = './lib/index.js'
+const name = 'ReactInfiniteRange'
 const globals = {
-  react: 'React',
-};
+  react: 'React'
+}
 
 // treat as external "module/path" modules and reserved rollup paths
 const external = id =>
-  !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/');
+  !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/')
 
 const getBabelOptions = ({ useESModules }) => ({
   exclude: '**/node_modules/**',
   runtimeHelpers: true,
-  plugins: [['@babel/transform-runtime', { useESModules }]],
-});
+  plugins: [['@babel/transform-runtime', { useESModules }]]
+})
 
 const commonjsOptions = {
-  include: '**/node_modules/**',
-};
+  include: '**/node_modules/**'
+}
 
 export default [
   {
     input,
-    output: { file: 'dist/react-infinite-range.js', format: 'umd', name, globals },
+    output: {
+      file: 'dist/react-infinite-range.js',
+      format: 'umd',
+      name,
+      globals
+    },
     external: Object.keys(globals),
     plugins: [
       nodeResolve(),
       babel(getBabelOptions({ useESModules: true })),
       commonjs(commonjsOptions),
-      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
-      sizeSnapshot(),
-    ],
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') })
+    ]
   },
 
   {
     input,
-    output: { file: 'dist/react-infinite-range.min.js', format: 'umd', name, globals },
+    output: {
+      file: 'dist/react-infinite-range.min.js',
+      format: 'umd',
+      name,
+      globals
+    },
     external: Object.keys(globals),
     plugins: [
       nodeResolve(),
       babel(getBabelOptions({ useESModules: true })),
       commonjs(commonjsOptions),
       replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      uglify(),
-    ],
+      uglify()
+    ]
   },
 
   {
     input,
     output: { file: pkg.module, format: 'esm' },
     external,
-    plugins: [babel(getBabelOptions({ useESModules: true }))],
+    plugins: [babel(getBabelOptions({ useESModules: true }))]
   },
 
   {
     input,
     output: { file: pkg.main, format: 'cjs' },
     external,
-    plugins: [babel(getBabelOptions({ useESModules: false }))],
-  },
-];
+    plugins: [babel(getBabelOptions({ useESModules: false }))]
+  }
+]
