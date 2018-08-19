acdc0e1e653f7f322ee91801b16779d28268557f
Eduard Kyvenko <edu@june.dk>
2018-08-19 09:24:05 +0200

    Setup the build pipeline

---
+++ rollup.config.js
@@ -0,0 +1,70 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import { uglify } from 'rollup-plugin-uglify';
+import pkg from './package.json';
+
+const input = './lib/index.js';
+const name = 'ReactInfiniteRange';
+const globals = {
+  react: 'React',
+};
+
+// treat as external "module/path" modules and reserved rollup paths
+const external = id =>
+  !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/');
+
+const getBabelOptions = ({ useESModules }) => ({
+  exclude: '**/node_modules/**',
+  runtimeHelpers: true,
+  plugins: [['@babel/transform-runtime', { useESModules }]],
+});
+
+const commonjsOptions = {
+  include: '**/node_modules/**',
+};
+
+export default [
+  {
+    input,
+    output: { file: 'dist/react-infinite-range.js', format: 'umd', name, globals },
+    external: Object.keys(globals),
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions({ useESModules: true })),
+      commonjs(commonjsOptions),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      sizeSnapshot(),
+    ],
+  },
+
+  {
+    input,
+    output: { file: 'dist/react-infinite-range.min.js', format: 'umd', name, globals },
+    external: Object.keys(globals),
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions({ useESModules: true })),
+      commonjs(commonjsOptions),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      sizeSnapshot(),
+      uglify(),
+    ],
+  },
+
+  {
+    input,
+    output: { file: pkg.module, format: 'esm' },
+    external,
+    plugins: [babel(getBabelOptions({ useESModules: true })), sizeSnapshot()],
+  },
+
+  {
+    input,
+    output: { file: pkg.main, format: 'cjs' },
+    external,
+    plugins: [babel(getBabelOptions({ useESModules: false }))],
+  },
+];
